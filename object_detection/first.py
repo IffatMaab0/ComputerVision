191efd8b891b46666e77ce7bef68fd5854a37efc
IffatMaab0 <iffatmaab11@gmail.com>
2026-08-12 12:44:00 +0500

    modified

--- object_detection/first.py
+++ object_detection/first.py
@@ -3,4 +3,18 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.pt")
 
 results = model("images/cat.webp")
-results[0].show()
+result = results[0]
+# print('Boxes')
+# print(result.boxes.xyxy)
+# print('Confidences')
+# print(result.boxes.conf)
+# print('Classes')
+# print(result.boxes.cls)
+for box in result.boxes:
+
+    class_id = int(box.cls[0])
+    confidence = float(box.conf[0])
+
+    class_name = model.names[class_id]
+
+    print(class_name, confidence)
